ba904964f7fc831fe33748b57939798dc9e45d89
Pascal Viaud <viaud@qualixel.fr>
2025-02-22 11:08:45 +0100

    Forcer redeploiement

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Forcer le redéploiement
 from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
