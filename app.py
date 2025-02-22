a70ab0a3200c774c0c9c89614c8b604c7cb201e2
Pascal Viaud <viaud@qualixel.fr>
2025-02-22 12:16:24 +0100

    Forcer le redeploiement avec une mise à jour de app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Forcer le redéploiement
+# Forcer le redéploiement sur Railway
 from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
